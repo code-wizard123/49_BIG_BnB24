83ac50d73f050b39933663e0ac8c2db72cadbc66
code-wizard123 <rgkalsi_b21@it.vjti.ac.in>
2024-02-17 23:58:56 +0530

    Background Changes

--- frontend/src/Components/NFTCard/NFTCard.jsx
+++ frontend/src/Components/NFTCard/NFTCard.jsx
@@ -1,46 +1,46 @@
 // NFTItem.js
-import React from 'react';
-import styles from './NFTItem.module.css';
-import { Button } from '@mui/material';
+import React from "react";
+import styles from "./NFTItem.module.css";
+import { Button } from "@mui/material";
 
 const NFTCard = () => {
-    return (
-        <div className={styles.container}>
-            <div className={styles.nftGridItem}>
-                <div className={styles.nftItemStyle}>
-                    <div className={styles.nftThumb}>
-                        <a href="#">
-                            <img
-                                src="https://thumbor.forbes.com/thumbor/fit-in/x/https://www.forbes.com/advisor/in/wp-content/uploads/2022/03/monkey-g412399084_1280.jpg"
-                                alt=""
-                            />
-                        </a>
-                        <button className={styles.nftReactionBtn}>
-                            <i className={styles.riHeartFill}></i>
-                            <span>69</span>
-                        </button>
-                    </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.nftGridItem}>
+        <div className={styles.nftItemStyle}>
+          <div className={styles.nftThumb}>
+            <a href="#">
+              <img
+                src="https://thumbor.forbes.com/thumbor/fit-in/x/https://www.forbes.com/advisor/in/wp-content/uploads/2022/03/monkey-g412399084_1280.jpg"
+                alt=""
+              />
+            </a>
+            <button className={styles.nftReactionBtn}>
+              <i className={styles.riHeartFill}></i>
+              <span>69</span>
+            </button>
+          </div>
 
-                    <div className={styles.nftContent}>
-                        <h3 className={styles.nftTitle} >
-                            <a href="#">Name NFT #1</a>
-                        </h3>
+          <div className={styles.nftContent}>
+            <h3 className={styles.nftTitle}>
+              <a href="#">Name NFT #1</a>
+            </h3>
 
-                        <div className={`${styles.nftProfile} ${styles.dFlexCenter}`}>
-                            <a href="#">
-                                <img
-                                    src="https://yt3.ggpht.com/QjvZEAyWwOGtkY4D36Q8tiblGg3vXwaohmUkr_zMNGatyLEniLkWq_MRpC7RaZNtoXbbcvC5tw=s900-c-k-c0x00ffffff-no-rj"
-                                    className={styles.nftImageAvatar}
-                                    alt=""
-                                />
-                            </a>
-                            <a href="#" className={styles.authortext}>
-                                @author_name
-                            </a>
-                            <i className={styles.riShieldCheckFill}></i>
-                        </div>
+            <div className={`${styles.nftProfile} ${styles.dFlexCenter}`}>
+              <a href="#">
+                <img
+                  src="https://yt3.ggpht.com/QjvZEAyWwOGtkY4D36Q8tiblGg3vXwaohmUkr_zMNGatyLEniLkWq_MRpC7RaZNtoXbbcvC5tw=s900-c-k-c0x00ffffff-no-rj"
+                  className={styles.nftImageAvatar}
+                  alt=""
+                />
+              </a>
+              <a href="#" className={styles.authortext}>
+                @author_name
+              </a>
+              <i className={styles.riShieldCheckFill}></i>
+            </div>
 
-                        {/* <div className={`${styles.nftProductOwner} ${styles.dFlexCenter}`}>
+            {/* <div className={`${styles.nftProductOwner} ${styles.dFlexCenter}`}>
                             <span className={styles.nftBidOwner}>
                                 PRICE:
                             </span>
@@ -50,24 +50,24 @@ const NFTCard = () => {
                             </span>
                         </div> */}
 
-                        <div className={`${styles.nftProductBuy} ${styles.dFlexBetween}`}>
-                            <span className={styles.nftBidOwner}>
-                                PRICE: <strong>99.1 ETH</strong>
-                            </span>
-                            {/* <button href="#" className={`${styles.nftBtn} ${styles.nftBtnCart} ${styles.nftBtnOutline}`}>
+            <div className={`${styles.nftProductBuy} ${styles.dFlexBetween}`}>
+              <span className={styles.nftBidOwner}>
+                PRICE: <strong>99.1 ETH</strong>
+              </span>
+              {/* <button href="#" className={`${styles.nftBtn} ${styles.nftBtnCart} ${styles.nftBtnOutline}`}>
                                 <span>
                                     <i className={styles.riShoppingCartLine}></i> Buy
                                 </span>
                             </button> */}
-                            <Button variant="contained" size="small">
-                                Buy
-                            </Button>
-                        </div>
-                    </div>
-                </div>
+              <Button variant="contained" size="small">
+                Buy
+              </Button>
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default NFTCard;
--- frontend/src/Pages/Marketplace.jsx
+++ frontend/src/Pages/Marketplace.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import Navbar from "../Components/Navbar/Navbar";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { useTheme } from '@mui/material/styles';
-import { Carousel } from 'react-responsive-carousel';
-import { Grid, Card, CardMedia, CardContent, Typography, Container, Box, Paper } from '@mui/material';
-import IconButton from '@mui/material/IconButton';
-import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
-import SkipNextIcon from '@mui/icons-material/SkipNext';
-import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
-import { Button, CardActionArea, CardActions } from '@mui/material';
-import NFTCard from '../Components/NFTCard/NFTCard';
-import './Marketplace.css'
-
+import {
+  Grid,
+  Card,
+  CardMedia,
+  Typography,
+  Box,
+} from "@mui/material";
+import PlayCircleOutlineIcon from "@mui/icons-material/PlayCircleOutline";
+import { Button, CardActions } from "@mui/material";
+import NFTCard from "../Components/NFTCard/NFTCard";
+import "./Marketplace.css";
 
 const ProductCard = ({ imageUrl, title, price }) => (
   <Card sx={{ padding: "0px" }}>
@@ -32,8 +31,8 @@ const ProductCard = ({ imageUrl, title, price }) => (
             ${price}
           </Typography>
         </Grid>
-        <Grid xs={6} item sx={{ justifyContent: 'center', display: 'flex' }}>
-          <PlayCircleOutlineIcon fontSize='large' />
+        <Grid xs={6} item sx={{ justifyContent: "center", display: "flex" }}>
+          <PlayCircleOutlineIcon fontSize="large" />
         </Grid>
         <Grid item xs={12} sx={{ justifyContent: "center", display: "flex" }}>
           <CardActions>
@@ -51,14 +50,13 @@ const ProductCard = ({ imageUrl, title, price }) => (
 );
 
 const Marketplace = () => {
-  const theme = useTheme();
   return (
     <div>
       <Navbar />
-      <div className='blurredFilter'>
-        <div className='beautifulBackground'>
+      <div className="blurredFilter">
+        <div className="beautiful-background">
           <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-            <div class='line-text'>Some Trending Songs</div>
+            <div className="line-text">Some Trending Songs</div>
             <Grid>
               {/* <Grid>
               <Card sx={{ display: 'flex', justifyContent: 'center', bgcolor: 'black' }}>
@@ -92,18 +90,27 @@ const Marketplace = () => {
               </Card>
             </Grid> */}
               <Grid>
-                <Grid sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', columnGap: '5vw' }}>
-                  {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map((index) => (
-                    <Grid item key={index} sx={{ padding: '0px' }}>
-                      <NFTCard />
-                    </Grid>
-                  ))}
+                <Grid
+                  sx={{
+                    display: "flex",
+                    flexWrap: "wrap",
+                    justifyContent: "center",
+                    columnGap: "5vw",
+                  }}
+                >
+                  {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map(
+                    (index) => (
+                      <Grid item key={index} sx={{ padding: "0px" }}>
+                        <NFTCard />
+                      </Grid>
+                    )
+                  )}
                 </Grid>
               </Grid>
             </Grid>
           </div>
         </div>
-      </div >
+      </div>
     </div>
   );
 };
