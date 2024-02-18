a5a964baa436c5413f04b7b854375d8e544fc257
code-wizard123 <rgkalsi_b21@it.vjti.ac.in>
2024-02-18 11:26:16 +0530

    Song Card

--- frontend/src/Components/NFTCard/NFTCard.jsx
+++ frontend/src/Components/NFTCard/NFTCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import styles from "./NFTItem.module.css";
 import { Button } from "@mui/material";
 
-const NFTCard = ({ id }) => {
+const NFTCard = ({ id, imgUrl, author, price }) => {
   const redirectUrl = `/auction/${id}`;
 
   return (
@@ -12,10 +12,7 @@ const NFTCard = ({ id }) => {
         <div className={styles.nftItemStyle}>
           <div className={styles.nftThumb}>
             <a href={redirectUrl}>
-              <img
-                src="https://thumbor.forbes.com/thumbor/fit-in/x/https://www.forbes.com/advisor/in/wp-content/uploads/2022/03/monkey-g412399084_1280.jpg"
-                alt=""
-              />
+              <img src={imgUrl} alt="" />
             </a>
             <button className={styles.nftReactionBtn}>
               <i className={styles.riHeartFill}></i>
@@ -37,13 +34,13 @@ const NFTCard = ({ id }) => {
                 />
               </a>
               <a href="#" className={styles.authortext}>
-                @author_name
+                @{author}
               </a>
               <i className={styles.riShieldCheckFill}></i>
             </div>
 
             <div className={`${styles.nftProductBuy} ${styles.dFlexBetween}`}>
-              Price: <strong>99.1 ETH</strong>
+              Price: <strong>{price}</strong>
             </div>
           </div>
         </div>
--- frontend/src/Pages/Marketplace.jsx
+++ frontend/src/Pages/Marketplace.jsx
@@ -10,81 +10,84 @@ import { GetIpfsUrlFromPinata } from "../utils";
 import axios from "axios";
 
 const Marketplace = () => {
-    const [data, updateData] = useState();
-    const [dataFetched, updateFetched] = useState(false);
+  const [data, updateData] = useState();
+  const [dataFetched, updateFetched] = useState(false);
 
-    async function getAllNFTs() {
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-        const signer = provider.getSigner();
-        let contract = new ethers.Contract(
-            MarketplaceJSON.address,
-            MarketplaceJSON.abi,
-            signer
-        );
-        //create an NFT Token
-        let transaction = await contract.getAllNFTs();
-        console.log(1, transaction);
-
-        //Fetch all the details of every NFT from the contract and display
-        const items = await Promise.all(
-            transaction.map(async (i) => {
-                var tokenURI = await contract.tokenURI(i.tokenId);
-                console.log("getting this tokenUri", tokenURI);
-                tokenURI = GetIpfsUrlFromPinata(tokenURI);
-                console.log("getting this ", tokenURI);
-                let meta = await axios.get(tokenURI);
-                meta = meta.data;
-
-                let price = ethers.utils.formatUnits(
-                    i.price.toString(),
-                    "ether"
-                );
-                let item = {
-                    price,
-                    tokenId: i.tokenId.toNumber(),
-                    seller: i.seller,
-                    owner: i.owner,
-                    image: meta.image,
-                    name: meta.name,
-                    description: meta.description,
-                };
-                return item;
-            })
-        );
-        // console.log(transaction);
-        console.log(items);
-
-        updateFetched(true);
-        updateData(items);
-    }
-
-    if (!dataFetched) getAllNFTs();
-    return (
-        <div>
-            <Navbar />
-            <div className="blurredFilter">
-                <div className="beautiful-background">
-                    <div className="mx-auto max-w-2xl py-16 px-4 sm:py-10 sm:px-6 lg:max-w-7xl lg:px-8">
-                        <div className="line-text">Marketplace</div>
-                        <Grid
-                            sx={{
-                                display: "flex",
-                                flexWrap: "wrap",
-                                justifyContent: "center",
-                                columnGap: "5rem",
-                            }}
-                        >
-                            {data.map((index) => (
-                                <Grid item key={index} sx={{ padding: "0px" }}>
-                                    <NFTCard id={index} />
-                                </Grid>
-                            ))}
-                        </Grid>
-                    </div>
-                </div>
-            </div>
-        </div>
+  async function getAllNFTs() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    let contract = new ethers.Contract(
+      MarketplaceJSON.address,
+      MarketplaceJSON.abi,
+      signer
     );
+    //create an NFT Token
+    let transaction = await contract.getAllNFTs();
+    console.log(1, transaction);
+
+    //Fetch all the details of every NFT from the contract and display
+    const items = await Promise.all(
+      transaction.map(async (i) => {
+        var tokenURI = await contract.tokenURI(i.tokenId);
+        console.log("getting this tokenUri", tokenURI);
+        tokenURI = GetIpfsUrlFromPinata(tokenURI);
+        console.log("getting this ", tokenURI);
+        let meta = await axios.get(tokenURI);
+        meta = meta.data;
+
+        let price = ethers.utils.formatUnits(i.price.toString(), "ether");
+        let item = {
+          price,
+          tokenId: i.tokenId.toNumber(),
+          seller: i.seller,
+          owner: i.owner,
+          image: meta.image,
+          name: meta.name,
+          description: meta.description,
+        };
+        return item;
+      })
+    );
+    // console.log(transaction);
+    console.log(items);
+
+    updateFetched(true);
+    updateData(items);
+  }
+
+  if (!dataFetched) getAllNFTs();
+  return (
+    <div>
+      <Navbar />
+      <div className="blurredFilter">
+        <div className="beautiful-background">
+          <div className="mx-auto max-w-2xl py-16 px-4 sm:py-10 sm:px-6 lg:max-w-7xl lg:px-8">
+            <div className="line-text">Marketplace</div>
+            <Grid
+              sx={{
+                display: "flex",
+                flexWrap: "wrap",
+                justifyContent: "center",
+                columnGap: "5rem",
+              }}
+            >
+              {data &&
+                data.map((value, index) => (
+                  <Grid item key={index} sx={{ padding: "0px" }}>
+                    <NFTCard
+                      id={value.tokenId}
+                      price={value.price}
+                      author={value.seller}
+                      imgUrl={value.image}
+                    />
+                  </Grid>
+                ))}
+            </Grid>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Marketplace;
